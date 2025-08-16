5a1b1f8455793c964766aded243ca59bf9ce9730
Sandro <17152022+sandro-git@users.noreply.github.com>
2025-08-16 16:48:47 +0200

    feat: add Partytown integration for improved performance and update Google Tag Manager implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,20 +8,19 @@ import tailwindcss from '@tailwindcss/vite';
 
 import netlify from '@astrojs/netlify';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity(
-      {
-        projectId: '0oshw5tf',
-        dataset: 'production',
-        useCdn: true, // See note on using the CDN
-        apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
-        studioBasePath: '/studio' // If you want to access the Studio on a route
+  integrations: [sanity(
+    {
+      projectId: '0oshw5tf',
+      dataset: 'production',
+      useCdn: true, // See note on using the CDN
+      apiVersion: "2025-01-28", // insert the current date to access the latest version of the API
+      studioBasePath: '/studio' // If you want to access the Studio on a route
 
-      }),
-    react()
-  ],
+    }), react(), partytown()],
 
   vite: {
     plugins: [
